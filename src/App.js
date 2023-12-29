25a6124f010da90e565f6cf19be4dd88ec7fd74e
Aarif-shazZ <shazaarif0@gmail.com>
2023-12-29 17:20:37 +0530

    done

--- src/App.js
+++ src/App.js
@@ -4,15 +4,32 @@ import Navbar from './Components/Navbar/Navbar';
 import Rightbar from './Components/Rightbar/Rightbar';
 import Sidebar from './Components/Sidebar/Sidebar';
 import { Box, Stack } from "@mui/material"
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// Define your theme with breakpoints
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+});
+
 function App() {
   return (
     <Box>
+      <ThemeProvider theme={theme}>
       <Navbar/>
       <Stack direction={"row"} spacing={2} justifyContent={"space-between"}>
         <Sidebar />
         <Feed />
         <Rightbar />
       </Stack>
+      </ThemeProvider>
     </Box>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from '@emotion/styled'
-import { AppBar, Badge, InputBase, Toolbar, Typography } from '@mui/material'
+import { AppBar, Badge, InputBase, Toolbar, Typography,Avatar,Box } from '@mui/material'
 import Instagram from "@mui/icons-material/Instagram"
 import Mail from '@mui/icons-material/Mail';
 import Notifications from '@mui/icons-material/Notifications';
@@ -11,11 +11,24 @@ const StyledToolbar = styled(Toolbar)({
 const Search = styled("div")(({ theme }) => ({
   backgroundColor: "white",
   padding: "0 10px",
-  borderRadius: "5px",
+  borderRadius: theme.shape.borderRadius,
   width: "40%"
 }))
 const Icons = styled("div")(({ theme }) => ({
-  // backgroundColor:"white"  
+  display:"none",
+  alignItems:"center",
+  gap:"20px",
+  [theme.breakpoints.up("sm")]:{
+    display:"flex"
+  }
+}))
+const UserBox = styled(Box)(({ theme }) => ({
+  display:"flex",
+  alignItems:"center",
+  gap:"10px",
+  [theme.breakpoints.up("sm")]:{
+    display:"none"
+  }
 }))
 const Navbar = () => {
   return (
@@ -31,7 +44,12 @@ const Navbar = () => {
           <Badge badgeContent={4} color={"error"}>
             <Notifications/>
           </Badge>
+          <Avatar sx={{width:30,height:30}} alt="Remy Sharp" src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D" />
         </Icons>
+        <UserBox>
+        <Avatar sx={{width:30,height:30}} alt="Remy Sharp" src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D" />
+        <Typography variant='span'>Aarif</Typography>
+        </UserBox>
       </StyledToolbar>
     </AppBar>
   )
